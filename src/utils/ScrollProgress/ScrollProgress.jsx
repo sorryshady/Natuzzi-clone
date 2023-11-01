e8061d36dd19b41d3a29e88e5a8219866b959b54
sorryShady <akhilnishadofficial@gmail.com>
2023-11-01 08:50:21 +0530

    slight css changes

--- src/utils/ScrollProgress/ScrollProgress.jsx
+++ src/utils/ScrollProgress/ScrollProgress.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSpring, useScroll, motion } from 'framer-motion'
 import styles from './ScrollProgress.module.css'
+import { useSelector } from 'react-redux'
 const ScrollProgress = () => {
   const { scrollYProgress } = useScroll()
   const scaleX = useSpring(scrollYProgress, {
@@ -8,9 +9,13 @@ const ScrollProgress = () => {
     damping: 30,
     restDelta: 0.001,
   })
+  const { active } = useSelector((state) => state.menu)
   return (
     <>
-      <motion.div className={styles['progress-bar']} style={{ scaleX }} />
+      <motion.div
+        className={`${styles['progress-bar']} ${active ? styles.hidden : ''}`}
+        style={{ scaleX }}
+      />
       {/* <div className={styles['progress-bar']}></div> */}
     </>
   )
